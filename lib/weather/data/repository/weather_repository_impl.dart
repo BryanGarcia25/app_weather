c9cc5b57d9b6b9830ec1fb82d66b699c494e1334
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-02 15:50:15 -0700

    Creating the repository to implement the methods that will be used

---
+++ lib/weather/data/repository/weather_repository_impl.dart
@@ -0,0 +1,31 @@
+import 'package:app_weather/core/error/failure.dart';
+import 'package:app_weather/weather/data/data_sources/weather_remote_data.dart';
+import 'package:app_weather/weather/domain/entity/weather.dart';
+import 'package:app_weather/weather/domain/repositoy/weather_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class WeatherRepositoryImpl extends WeatherRepository {
+  final WeatherRemoteData weatherRemoteData;
+  WeatherRepositoryImpl({required this.weatherRemoteData});
+
+  @override
+  Future<Either<Failure, Weather>> getWeatherByCityName(String cityName) async {
+    try {
+      final Weather resp = await weatherRemoteData.getWeatherByCityName(cityName);
+      return Right(resp);
+    } on ServerFailure {
+      return Left(ServerFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, Weather>> getWeatherByCoordinates(double latitude, double longitude) async {
+    try {
+      final Weather resp = await weatherRemoteData.getWeatherByCoordinates(latitude, longitude);
+      return Right(resp);
+    } on ServerFailure {
+      return Left(ServerFailure());
+    }
+  }
+  
+}
---
+++ lib/weather/domain/repositoy/weather_repository.dart
@@ -0,0 +1,8 @@
+import 'package:app_weather/core/error/failure.dart';
+import 'package:app_weather/weather/domain/entity/weather.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class WeatherRepository {
+  Future<Either<Failure, Weather>> getWeatherByCoordinates(double latitude, double longitude);
+  Future<Either<Failure, Weather>> getWeatherByCityName(String cityName);
+}
