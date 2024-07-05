929a14e87aaff318f4a149df058b0a3165276ef8
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-05 01:05:24 -0700

    Registering the RemoteWeatherBloc class to GetIt

--- lib/injection_dependencies.dart
+++ lib/injection_dependencies.dart
@@ -3,11 +3,13 @@ import 'package:app_weather/weather/data/repository/weather_repository_impl.dart
 import 'package:app_weather/weather/domain/repositoy/weather_repository.dart';
 import 'package:app_weather/weather/domain/use_cases/get_weather_by_city.dart';
 import 'package:app_weather/weather/domain/use_cases/get_weather_by_coordinates.dart';
+import 'package:app_weather/weather/presentation/BLoC/remote_weather_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 final getItInstance = GetIt.instance;
 
 Future<void> initializeDependencies() async {
+  getItInstance.registerFactory(() => RemoteWeatherBloc(getItInstance()));
 
   getItInstance.registerLazySingleton(() => GetWeatherByCityNameUseCase(weatherRepository: getItInstance()));
   getItInstance.registerLazySingleton(() => GetWeatherByCoordinatesUseCase(weatherRepository: getItInstance()));
