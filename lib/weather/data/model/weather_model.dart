131145a2e7cdd4129850d9bac5d9cb89a4c2ae0b
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-05 00:21:53 -0700

    Editing the fields to obtain the data

--- lib/weather/data/model/weather_model.dart
+++ lib/weather/data/model/weather_model.dart
@@ -7,16 +7,16 @@ class WeatherModel extends Weather {
   factory WeatherModel.fromJson(json) {
     return WeatherModel(
       cityName : json['name'],
-      main : json['weather']['main'],
-      description : json['weather']['description'],
-      temp : json['main']['temp'],
-      feelsLike: json['main']['feels_like'],
-      tempMax : json['main']['tempMax'],
-      tempMin : json['main']['tempMin'],
+      main : json['weather'][0]['main'],
+      description : json['weather'][0]['description'],
+      temp : double.parse((json['main']['temp'] - 273.15).toString()).toInt(),
+      feelsLike: double.parse((json['main']['feels_like'] - 273.15).toString()).toInt(),
+      tempMax : double.parse((json['main']['temp_max'] - 273.15).toString()).toInt(),
+      tempMin : double.parse((json['main']['temp_min'] - 273.15).toString()).toInt(),
       humidity : json['main']['humidity'],
       visibility : json['visibility'],
       windSpeed : json['wind']['speed'],
-      rain : json['rain']['1h'],
+      rain : 0.0,
       cloudPorcentage : json['clouds']['all'],
       sunrise : json['sys']['sunrise'],
       sunset : json['sys']['sunset']
