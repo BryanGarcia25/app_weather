6c6dd4fc119ebe6cd0027ac4cb086987431fbb26
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-05 00:40:31 -0700

    Creating the injection_dependences.dart file

---
+++ lib/injection_dependencies.dart
@@ -0,0 +1,18 @@
+import 'package:app_weather/weather/data/data_sources/weather_remote_data.dart';
+import 'package:app_weather/weather/data/repository/weather_repository_impl.dart';
+import 'package:app_weather/weather/domain/repositoy/weather_repository.dart';
+import 'package:app_weather/weather/domain/use_cases/get_weather_by_city.dart';
+import 'package:app_weather/weather/domain/use_cases/get_weather_by_coordinates.dart';
+import 'package:get_it/get_it.dart';
+
+final getItInstance = GetIt.instance;
+
+Future<void> initializeDependencies() async {
+
+  getItInstance.registerLazySingleton(() => GetWeatherByCityNameUseCase(weatherRepository: getItInstance()));
+  getItInstance.registerLazySingleton(() => GetWeatherByCoordinatesUseCase(weatherRepository: getItInstance()));
+  
+  getItInstance.registerLazySingleton<WeatherRepository>(() => WeatherRepositoryImpl(weatherRemoteData: getItInstance()));
+
+  getItInstance.registerLazySingleton<WeatherRemoteData>(() => WeatherRemoteDataImpl());
+}
