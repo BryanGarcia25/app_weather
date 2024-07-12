011a7d32a0cdb2f7603400220ca48889125367b7
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-12 01:34:13 -0700

    Removing print

--- lib/config/theme/scaffold_background.dart
+++ lib/config/theme/scaffold_background.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 final date = DateTime.now();
 
-Color backgroundColor(String sunset) {
-  print(date.hour);
+Color backgroundColor() {
   return date.hour <= 6 || date.hour >= 20 ? const Color(0xFF131416) : const Color(0xFFffffff);
 }
