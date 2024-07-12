688f9687b8aedf654b52f3825cdf326d8ca4f8fc
BryanGarcia25 <osvaldo52programacion@gmail.com>
2024-07-12 01:34:40 -0700

    Removing banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (_) => GetIt.instance.get<RemoteWeatherBloc>())
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
